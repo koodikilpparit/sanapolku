83867008aa63c57a208ceaa87d4e2f4b8dc94a03
Eveliina Sundberg <eveliina.sundberg@tuni.fi>
2024-09-26 10:25:42 +0300

    #13 Add a dummy test to check that the App renders

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-//Tests
+test('renders App component and checks if it starts', () => {
+  render(<App />);
+  const appElement = screen.getByText(/Sanapolku/i);
+  expect(appElement).toBeInTheDocument();
+});
