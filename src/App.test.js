4cd43ef0e7b1549097cbc2ed9acbe831a557262a
Eveliina Sundberg <eveliina.sundberg@tuni.fi>
2024-09-26 10:12:36 +0300

    #13 Modify test to match implementation

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Start page by default', () => {
+  render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>
+  );
+  const startElement = screen.getByText(/Sanapolku/i);
+  expect(startElement).toBeInTheDocument();
 });
