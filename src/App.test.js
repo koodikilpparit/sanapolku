682c75af8de51e41df88e0c03d262512f7fcd6b8
Eveliina Sundberg <eveliina.sundberg@tuni.fi>
2024-09-26 10:20:30 +0300

    #13 Delete test :)

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,4 @@
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders Start page by default', () => {
-  render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>
-  );
-  const startElement = screen.getByText(/Sanapolku/i);
-  expect(startElement).toBeInTheDocument();
-});
+//Tests
