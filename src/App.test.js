929b60db7af4dea92a54c4019e62d01f9c178cb7
Anni Nieminen <anni.k.nieminen@tuni.fi>
2024-10-04 14:51:41 +0300

    #10 Testi menee läpi App-komponentin renderöityessä

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders App component and checks if it starts', () => {
+test('checks if App component renders', () => {
   render(<App />);
-  const appElement = screen.getByText(/Sanapolku/i);
-  expect(appElement).toBeInTheDocument();
 });
