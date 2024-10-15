f05c205b4dc5e57b2523e5e67487019612708c91
Eveliina Sundberg <eveliina.sundberg@tuni.fi>
2024-10-15 11:20:10 +0300

    Fix formatting

--- src/ImageUploader.js
+++ src/ImageUploader.js
@@ -1,12 +1,12 @@
 import './styles/NewWord.css';
 
-import {faImage} from '@fortawesome/free-solid-svg-icons';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faImage } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 // Component that allows uploading images
-const ImageUploader = ({setImageData}) => {
+const ImageUploader = ({ setImageData }) => {
   // State for uploaded images
   const [uploadedImage, setUploadedImage] = useState(null); // Store uploaded image for preview
 
@@ -30,13 +30,13 @@ const ImageUploader = ({setImageData}) => {
   };
 
   return (
-    <div className='image-upload-container'>
-      <label className='image-upload-button'>
-        <FontAwesomeIcon icon={faImage} className='image-icon' />
+    <div className="image-upload-container">
+      <label className="image-upload-button">
+        <FontAwesomeIcon icon={faImage} className="image-icon" />
         Lataa kuva
         <input
-          type='file'
-          accept='image/*'
+          type="file"
+          accept="image/*"
           onChange={handleFileUpload}
           style={{ display: 'none' }}
         />
@@ -45,14 +45,14 @@ const ImageUploader = ({setImageData}) => {
       {uploadedImage ? (
         <img
           src={uploadedImage}
-          alt='Esikatselu'
-          className='image-placeholder'
+          alt="Esikatselu"
+          className="image-placeholder"
         />
       ) : (
         <img
-          src='https://placehold.co/150x150'
-          alt='Placeholder'
-          className='image-placeholder'
+          src="https://placehold.co/150x150"
+          alt="Placeholder"
+          className="image-placeholder"
         />
       )}
     </div>
