3ada53bf0980d11bd9cd9587a6a3be32e7099b1d
Eveliina Sundberg <eveliina.sundberg@tuni.fi>
2024-10-16 19:51:52 +0300

    Ugh forgot to lint

--- src/pages/Instructions.test.js
+++ src/pages/Instructions.test.js
@@ -1,35 +1,34 @@
-import React from "react";
+import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import Start from './Start';
 import Instructions from './Instructions';
 
-describe("StartPage", () => {
-    // Rendering the instructions page
-    it('checks if Instructions-page renders', () => {
-        render(
-            <BrowserRouter>
-                <Instructions />
-            </BrowserRouter>
-        );       
-    });
+describe('StartPage', () => {
+  // Rendering the instructions page
+  it('checks if Instructions-page renders', () => {
+    render(
+      <BrowserRouter>
+        <Instructions />
+      </BrowserRouter>
+    );
+  });
 
-    it('checks if back-button brings you to the start page', () => {
-        // Rendering the required pages
-        const { container } = render(
-            <BrowserRouter>
-                <Instructions />
-                <Start />
-            </BrowserRouter>
-        );
-        
-        // Checking that the back-button is on the start page
-        const backButton = container.querySelector(".back-button");
-        expect(backButton).toBeInTheDocument();
-        
-        // Checking that by clicking the back-button you can navigate to /
-        fireEvent.click(backButton);
-        expect(window.location.pathname).toBe('/');
-    });
+  it('checks if back-button brings you to the start page', () => {
+    // Rendering the required pages
+    const { container } = render(
+      <BrowserRouter>
+        <Instructions />
+        <Start />
+      </BrowserRouter>
+    );
 
+    // Checking that the back-button is on the start page
+    const backButton = container.querySelector('.back-button');
+    expect(backButton).toBeInTheDocument();
+
+    // Checking that by clicking the back-button you can navigate to /
+    fireEvent.click(backButton);
+    expect(window.location.pathname).toBe('/');
+  });
 });
