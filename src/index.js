ed66cd2e0b76eee298776f8fae8683ad3cee745e
xqrima <risto.majakangas@tuni.fi>
2024-10-19 16:28:24 +0300

    Refactor index.js to remove unnecessary code

--- src/index.js
+++ src/index.js
@@ -12,8 +12,7 @@ root.render(
     <SettingsProvider>
       <App />
     </SettingsProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
