76d8e8d739dba5352f0c97a2e5d41935675e3148
Juho Rantala <juho.rantala@live.com>
2024-10-22 18:00:08 +0300

    Test publish-subscribe

--- src/pages/Share.js
+++ src/pages/Share.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './Start.css'; // Lol liian laiska tekee ees mitää css tiedostoo napille
+import SignalHub from 'signalhub';
+
+const hub = SignalHub('sanapolku', [
+  'https://signalhub-jccqtwhdwc.now.sh',
+  'https://signalhub-hzbibrznqa.now.sh',
+]);
 
 const handleShareClick = () => {
   console.log('Share');
+  hub.broadcast('my-channel', { hello: 'world' });
 };
 
 const handleReceiveClick = () => {
   console.log('Receive');
+  hub.subscribe('my-channel', (message) => {
+    console.log('Received', message);
+  });
 };
 
 const Share = () => {
