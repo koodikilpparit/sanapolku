70759a2b52f287e4b990549dc63ceb1b76be9961
Juho Rantala <juho.rantala@live.com>
2024-10-22 18:46:10 +0300

    Test sending data

--- src/pages/Share.js
+++ src/pages/Share.js
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Start.css'; // Lol liian laiska tekee ees mitää css tiedostoo napille
-
-const handleShareClick = () => {
-  console.log('Share');
-};
-
-const handleReceiveClick = () => {
-  console.log('Receive');
-};
+import Peer from 'peerjs';
 
 const Share = () => {
+  const [peerId, setPeerId] = useState('');
+  const [remoteId, setRemoteId] = useState('');
+  const [connection, setConnection] = useState(null);
+
+  const handleShareClick = () => {
+    console.log('Share');
+    connectToPeer();
+  };
+
+  const handleReceiveClick = () => {
+    console.log('Receive');
+  };
+
+  const connectToPeer = () => {
+    const conn = connection.connect(remoteId);
+    conn.on('open', () => {
+      conn.send('Hello from ' + peerId);
+    });
+  };
+
+  useEffect(() => {
+    const peer = new Peer();
+
+    peer.on('open', (id) => {
+      setPeerId(id);
+      console.log('My ID: ', id);
+    });
+
+    peer.on('connection', (conn) => {
+      conn.on('data', (data) => {
+        console.log('Received', data);
+      });
+    });
+
+    setConnection(peer);
+
+    return () => {
+      peer.destroy();
+    };
+  }, []);
+
   return (
     <div>
+      <input
+        type="text"
+        value={remoteId}
+        onChange={(e) => setRemoteId(e.target.value)}
+        placeholder="Enter remote peer ID"
+      />
       <button className="start-button" onClick={handleShareClick}>
         Testaa jakamista
       </button>
