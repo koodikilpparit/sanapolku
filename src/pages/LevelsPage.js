e81fd089c9130a4e4618a68d7b9041d825ceb5ce
Anni Nieminen <anni.k.nieminen@tuni.fi>
2024-10-25 16:52:00 +0300

    #22 Lisätty tasojen overview sivu

---
+++ src/pages/LevelsPage.js
@@ -0,0 +1,99 @@
+// LevelsPage.js
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { getPathByName, getWordsForPath } from '../db/db';
+
+const LevelsPage = () => {
+  const { pathName } = useParams();
+  const navigate = useNavigate();
+  const [levels, setLevels] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const wordsIntoLevels = (words) => {
+    const totalWords = words.length;
+    let levelCount = 0;
+    
+    // Determine the number of levels (min: 1, max: -)
+    if (totalWords <= 5) {
+      levelCount = totalWords;
+    }
+    else {
+      levelCount = Math.ceil(totalWords / 50) * 5;
+    }
+    
+    // Base number of words per level
+    const baseWordsPerLevel = Math.floor(totalWords / levelCount);
+    
+    // Extra words that need to be added
+    const extraWords = totalWords % levelCount;
+    
+    // Initialize levels
+    const levels = Array.from({ length: levelCount }, () => []);
+  
+    let wordIndex = 0;
+  
+    // Distribute the base number of words to each level
+    for (let i = 0; i < levelCount; i++) {
+      levels[i] = words.slice(wordIndex, wordIndex + baseWordsPerLevel);
+      wordIndex += baseWordsPerLevel;
+    }
+  
+    // Distribute the extra words starting from the first level towards the end
+    for (let i = 0; i < extraWords; i++) {
+      levels[i].push(words[wordIndex]);
+      wordIndex++;
+    }
+  
+    return levels;
+  };  
+
+  useEffect(() => {
+    const fetchLevels = async () => {
+      try {
+        const path = await getPathByName(pathName);
+        if (!path) {
+          setError('Path not found');
+          return;
+        }
+
+        const fetchedWords = await getWordsForPath(path.id);
+        if (!fetchedWords || fetchedWords.length === 0) {
+          setError('No words found for this path');
+          return;
+        }
+
+        const levels = wordsIntoLevels(fetchedWords);
+        setLevels(levels);
+        setLoading(false);
+      } catch (error) {
+        setError('Error fetching path or levels');
+        console.error(error);
+        setLoading(false);
+      }
+    };
+
+    fetchLevels();
+  }, [pathName]);
+
+  if (loading) return <p>Loading levels...</p>;
+  if (error) return <p>{error}</p>;
+
+  return (
+    <div className="levels-page">
+        {levels.map((level, index) => (
+            <button
+              onClick={() =>
+                navigate(`/peli/${pathName}/taso/${index + 1}`, {
+                  state: { levels, currentLevelIndex: index },
+                })
+              }
+            >
+              Level {index + 1}
+            </button>
+        ))}
+    </div>
+  );
+};
+
+export default LevelsPage;
