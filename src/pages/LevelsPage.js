764200c39230099756e1e52852a2403ca0b02da9
Anni Nieminen <anni.k.nieminen@tuni.fi>
2024-10-27 12:25:01 +0200

    #22 Korjattu tyyli-ongelmia

--- src/pages/LevelsPage.js
+++ src/pages/LevelsPage.js
@@ -13,7 +13,13 @@ import './LevelsPage.css';
 
 const levelButtons = [PawButton, RunButton, TreeButton, BriefCaseButton];
 const finalButton = TrophyButton;
-const positions = [[130, 180], [240, 360], [340, 580], [240, 780], [130, 980]];
+const positions = [
+  [130, 180],
+  [240, 360],
+  [340, 580],
+  [240, 780],
+  [130, 980],
+];
 
 const LevelsPage = () => {
   const { pathName } = useParams();
@@ -25,40 +31,39 @@ const LevelsPage = () => {
   const wordsIntoLevels = (words) => {
     const totalWords = words.length;
     let levelCount = 0;
-    
+
     // Determine the number of levels (min: 1, max: -)
     if (totalWords <= 5) {
       levelCount = totalWords;
-    }
-    else {
+    } else {
       levelCount = Math.ceil(totalWords / 50) * 5;
     }
-    
+
     // Base number of words per level
     const baseWordsPerLevel = Math.floor(totalWords / levelCount);
-    
+
     // Extra words that need to be added
     const extraWords = totalWords % levelCount;
-    
+
     // Initialize levels
     const levels = Array.from({ length: levelCount }, () => []);
-  
+
     let wordIndex = 0;
-  
+
     // Distribute the base number of words to each level
     for (let i = 0; i < levelCount; i++) {
       levels[i] = words.slice(wordIndex, wordIndex + baseWordsPerLevel);
       wordIndex += baseWordsPerLevel;
     }
-  
+
     // Distribute the extra words starting from the first level towards the end
     for (let i = 0; i < extraWords; i++) {
       levels[i].push(words[wordIndex]);
       wordIndex++;
     }
-  
+
     return levels;
-  };  
+  };
 
   useEffect(() => {
     const fetchLevels = async () => {
@@ -97,44 +102,41 @@ const LevelsPage = () => {
         <BackButton className="levels-back-button" />
       </div>
       <div className="path-container">
-        <Path className="path"/>
+        <Path className="path" />
         {levels.map((level, index) => {
           const ButtonComponent =
             index === levels.length - 1
               ? finalButton
               : levelButtons[Math.floor(Math.random() * levelButtons.length)];
 
-            const position = positions[index];
+          const position = positions[index];
 
-            return (
-              <ButtonComponent
-                key={index}
-                onClick={() =>
-                  navigate(`/peli/${pathName}/taso/${index + 1}`)
-                }
-                style={{
-                  position: 'absolute',
-                  top: `${position[0]}px`,
-                  left: `${position[1]}px`,
-                  zIndex: '1',
-                  width: '131.579px',
-                  height: '141.228px',
-                  borderRadius: '50%',
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  fontSize: '60px',
-                  background: '#72A895',
-                  color: '#06705b',
-                  boxShadow: '#06705b',
-                  border: 'none',
-                  boxShadow: '0 10px 0 rgba(0, 0, 0, 0.3)',
-                  transition: 'box-shadow 0.3s ease',
-                  cursor: 'pointer',
-                }}
-              />
-            );
-          })}
+          return (
+            <ButtonComponent
+              key={index}
+              onClick={() => navigate(`/peli/${pathName}/taso/${index + 1}`)}
+              style={{
+                position: 'absolute',
+                top: `${position[0]}px`,
+                left: `${position[1]}px`,
+                zIndex: '1',
+                width: '131.579px',
+                height: '141.228px',
+                borderRadius: '50%',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                fontSize: '60px',
+                background: '#72A895',
+                color: '#06705b',
+                border: 'none',
+                boxShadow: '0 10px 0 rgba(0, 0, 0, 0.3)',
+                transition: 'box-shadow 0.3s ease',
+                cursor: 'pointer',
+              }}
+            />
+          );
+        })}
       </div>
     </div>
   );
