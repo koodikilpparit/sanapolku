5437df714b69fa63b07de0df3531eaade7a1ea0d
Salonj <124079532+Salonj@users.noreply.github.com>
2024-11-05 01:08:44 +0200

    Phase2 toteutettu

--- src/components/game/Phase2.js
+++ src/components/game/Phase2.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import './Phase1.css';
+import ImageContainer from './ImageContainer';
 
 const Phase2 = ({
   currentWord,
@@ -7,24 +9,99 @@ const Phase2 = ({
   playerInput,
   handleInputChange,
   handleSubmit,
-}) => (
-  <div>
-    <h1>Järjestä kirjaimet</h1>
-    <img src={currentWord.img} alt={`Kuva sanasta ${currentWord.word}`} />
-    <p>{shuffledWord.toUpperCase()}</p>
-    <div className="submit-word-div">
-      <label htmlFor="player-input">Syötä sana:</label>
-      <input
-        id="player-input"
-        type="text"
-        value={playerInput.join('')}
-        onChange={handleInputChange}
-        placeholder="Syötä sana"
-      />
-      <button onClick={handleSubmit}>Valmis</button>
+  inputRefs,
+}) => {
+  const isReadyButtonDisabled = playerInput.some((letter) => letter === '');
+
+  useEffect(() => {
+    inputRefs.current = inputRefs.current.slice(0, currentWord.word.length);
+  }, [currentWord, inputRefs]);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === 'Enter' && !isReadyButtonDisabled) {
+        handleSubmit();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [isReadyButtonDisabled, handleSubmit]);
+
+  // Function to handle backspace key for navigating to the previous input
+  const handleBackspaceNavigation = (index, event) => {
+    if (event.key === 'Backspace' && index > 0 && playerInput[index] === '') {
+      inputRefs.current[index - 1].focus();
+    }
+  };
+
+  return (
+    <div className="flex flex-col h-full">
+      <h1 className="text-sp-white text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
+        Järjestä kirjaimet
+      </h1>
+      <div className="flex flex-col sm:flex-row mt-8 w-full">
+        <div className="w-full sm:w-1/2 flex items-center justify-center">
+          <ImageContainer
+            src={currentWord.img}
+            alt={`Kuva sanasta ${currentWord.word}`}
+            className=""
+          />
+        </div>
+
+        <div className="w-full sm:w-1/2 gap-16 sm:gap-0 flex flex-col mt-8 sm:mt-0 px-4 sm:px-0">
+          <div className="flex flex-col gap-2 sm:gap-4">
+            <div className="flex gap-1 w-full">
+              {shuffledWord
+                .toUpperCase()
+                .split('')
+                .map((letter, index) => (
+                  <div
+                    key={index}
+                    className="w-full aspect-square rounded-lg font-bold text-center text-2xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl bg-sp-white text-sp-black p-1 flex items-center justify-center"
+                  >
+                    {letter}
+                  </div>
+                ))}
+            </div>
+            <div className="flex gap-1 w-full">
+              {currentWord.word.split('').map((_, index) => (
+                <input
+                  key={index}
+                  type="text"
+                  ref={(el) => (inputRefs.current[index] = el)}
+                  value={playerInput[index] || ''}
+                  onChange={(event) =>
+                    handleInputChange(index, event, inputRefs)
+                  }
+                  onKeyDown={(event) => handleBackspaceNavigation(index, event)}
+                  maxLength="1"
+                  className="w-full aspect-square rounded-lg font-bold text-center text-2xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl bg-sp-white text-sp-black p-1"
+                />
+              ))}
+            </div>
+          </div>
+
+          <div className="mt-auto flex justify-end">
+            <button
+              className={`btn-sp-primary w-full sm:w-1/2 ${
+                isReadyButtonDisabled
+                  ? 'bg-sp-gray cursor-not-allowed'
+                  : 'bg-sp-light-green cursor-pointer'
+              }`}
+              onClick={handleSubmit}
+              disabled={isReadyButtonDisabled}
+            >
+              VALMIS
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 Phase2.propTypes = {
   currentWord: PropTypes.shape({
@@ -35,6 +112,7 @@ Phase2.propTypes = {
   playerInput: PropTypes.array.isRequired,
   handleInputChange: PropTypes.func.isRequired,
   handleSubmit: PropTypes.func.isRequired,
+  inputRefs: PropTypes.object.isRequired,
 };
 
 export default Phase2;
