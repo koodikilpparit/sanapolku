12533e7d21adfd92e081c13febec64e74cc771ca
Eveliina Sundberg <eveliina.sundberg@tuni.fi>
2024-11-08 08:24:51 +0200

    Get images from public

--- src/pages/Instructions.js
+++ src/pages/Instructions.js
@@ -2,9 +2,6 @@ import React from 'react';
 import BackButton from '../components/universal/BackButton';
 import InstructionsStep from '../components/instructions/InstructionsStep';
 import '../styles/Instructions.css';
-import phase1Img from '../assets/instruction-images/phase1.png';
-import phase2Img from '../assets/instruction-images/phase2.png';
-import phase3Img from '../assets/instruction-images/phase3.png';
 
 function Instructions() {
   return (
@@ -31,28 +28,28 @@ function Instructions() {
           stepNumber={1}
           title="Vaihe 1 – KUVA"
           text="Ensin yritetään muodostaa sana pelkän kuvan perusteella. Jos onnistut ensimmäisellä kerralla, niin etenet suoraan seuraavaan kuvaan. Jos sana ei muistu mieleen, niin mennään toiseen vaiheeseen, eli JÄRJESTÄ-osuuteen."
-          image={phase1Img}
+          image="/sanapolku/instruction-images/phase1.png"
         />
 
         <InstructionsStep
           stepNumber={2}
           title="Vaihe 2 – JÄRJESTÄ"
           text="Kuvan viereen ilmestyy kirjaimia, jotka tulee järjestää oikeaan järjestykseen. Painamalla VALMIS pääset eteenpäin."
-          image={phase2Img}
+          image="/sanapolku/instruction-images/phase2.png"
         />
 
         <InstructionsStep
           stepNumber={3}
           title="Vaihe 3 – KOPIOI"
           text="Kun oikea kirjoitusasu on näkyvissä, kopioi kirjaimista muodostunut sana."
-          image={phase3Img}
+          image="/sanapolku/instruction-images/phase3.png"
         />
 
         <InstructionsStep
           stepNumber={4}
           title="Vaihe 4 – TOISTA"
           text="Muistatko vielä vastauksen? Nyt TOISTA vaiheessa kirjoitetaan sana uudelleen. Oikeasta vastauksesta pääset seuraavaan sanaan ja väärästä vastauksesta palataan pelin JÄRJESTÄ-vaiheeseen."
-          image={phase1Img}
+          image="/sanapolku/instruction-images/phase1.png"
         />
       </div>
     </div>
