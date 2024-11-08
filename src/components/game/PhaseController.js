08e73002ddbb5d7bbbab49de58149706f2b4d897
Anni Nieminen <124594822+annikniem@users.noreply.github.com>
2024-11-08 09:20:55 +0200

    22 implement phase one for word in game (#142)
    * #22 Parannettu peli-sivun layouttia ja ulkonäköä
    
    * #22 Sanan syöttö -komponentti vaihdettu kirjainlaatoiksi
    
    * #22 Vaiheelle 1 luotu yksinkertainen tyyli
    
    * #22 Parannettu 1-vaiheen ja pelisivun komponenttien asettelua
    
    * #22 Paranneltu UI:ta ja lisätty skaalautuvuus eri näytön koille
    
    * #22 Korjattu pelin vaiheet 2 ja 3 toimimaan vaiheen 1 muutoksista huolimatta
    
    * #22 Korjattu tyyliongelmia
    
    * #22 Korjattu yksikkötestit toimimaan uuden input-tavan kanssa
    
    * #22 Lisätty yksikkötesti peli-sivun takaisin-napille
    
    * #22 Selkeytetty input käsittelyä
    
    * #22 Lisätty yksikkötestit pelin 1-vaiheelle
    
    * #22 Korjattu tyyliongelmia
    
    * #22 Korjattu vaiheen 1 input toimimaan puhelimella
    
    * #22 Päivitetty vaiheen 1 tyyliä
    
    * #22 Päivitetty pelin logiikka toimimaan vaiheen 1 kanssa
    
    * #22 Korjattu vaiheet 2 ja 3 toimimaan vaiheen 1 muutoksista riippumatta
    
    * #22 Korjattu testit toimimaan muutoksista huolimatta
    
    * #22 Korjattu tyyliongelmia
    
    * #22 Korjattu seuraavan kirjainlaatan automaattinen aktivoituminen sekä siistitty koodia
    
    * #22 Vastauksen lähetys estetty, jos kaikkia input fieldejä ei ole täytetty
    
    * #22 Korjattu vaiheen 1 yksikkötestejä
    
    * #22 Muokattu valmis-napin tyyliä riippumaan siitä, onko kaikki kirjaimet täytetty
    
    * #22 Korjattu tyyliongelmia enter-näppäimen käyttöön liittyen
    
    * #22 Lisätty yksikkötesti enter-näppäimen käytölle
    
    * Lisätty auto-focus ja korjattu backspacen toiminta
    
    * Jaettu phasen toiminta omaan komponenttiin
    
    * Lisätty oma komponentti kuville
    
    * Muokattu UI ja korjattu input
    
    * Korjattu backbutton testi
    
    * Korjattu errorit phase2
    
    * Paranneltu responsiivisuutta
    
    * 126 implement phase two for word in game (#146)
    
    * Phase2 toteutettu
    
    * Phase2 testit lisätty
    
    * Lisätty valmis UI
    
    * Phase2 toteutettu
    
    * Phase2 testit lisätty
    
    * Lisätty valmis UI
    
    * Paranneltu responsiivisyytta
    
    * Korjattu lettertilejen responsiivisuus
    
    * 138 implement phase three for word in game (#145)
    
    * Phase3 toteutettu
    
    * Phase3 testit lisätty
    
    * Phase3 UI tehty
    
    * Paranneltu phase3 responsiivisuutta
    
    ---------
    
    Co-authored-by: Salonj <124079532+Salonj@users.noreply.github.com>

---
+++ src/components/game/PhaseController.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Phase1 from './Phase1';
+import Phase2 from './Phase2';
+import Phase3 from './Phase3';
+const PhaseController = ({
+  currentPhase,
+  currentWord,
+  playerInput,
+  handleInputChange,
+  handleSubmit,
+  inputRefs,
+  shuffledWord = '',
+}) => {
+  switch (currentPhase) {
+    case 1:
+      return (
+        <Phase1
+          currentWord={currentWord}
+          playerInput={playerInput}
+          handleInputChange={handleInputChange}
+          handleSubmit={handleSubmit}
+          inputRefs={inputRefs}
+        />
+      );
+    case 2:
+      return (
+        <Phase2
+          currentWord={currentWord}
+          shuffledWord={shuffledWord}
+          playerInput={playerInput}
+          handleInputChange={handleInputChange}
+          handleSubmit={handleSubmit}
+          inputRefs={inputRefs}
+        />
+      );
+    case 3:
+      return (
+        <Phase3
+          currentWord={currentWord}
+          playerInput={playerInput}
+          handleInputChange={handleInputChange}
+          handleSubmit={handleSubmit}
+          inputRefs={inputRefs}
+        />
+      );
+    default:
+      return null;
+  }
+};
+
+PhaseController.propTypes = {
+  currentPhase: PropTypes.number.isRequired,
+  currentWord: PropTypes.shape({
+    img: PropTypes.string.isRequired,
+    word: PropTypes.string.isRequired,
+  }).isRequired,
+  playerInput: PropTypes.array.isRequired,
+  handleInputChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  inputRefs: PropTypes.object.isRequired,
+  shuffledWord: PropTypes.string,
+};
+
+export default PhaseController;
