91fd52dc20d978b4cecce927211e2edfaa67db40
xqrima <risto.majakangas@tuni.fi>
2024-11-09 14:19:16 +0200

    Refactor getAdultPath and getKidPath to remove redundant domain checks

--- src/db/StockPathHelper.js
+++ src/db/StockPathHelper.js
@@ -1,37 +1,21 @@
 import { adultImageNames, kidImageNames } from './imageNames';
 import shuffleArray from 'lodash.shuffle';
 
-function adultImagesPath() {
-  const domain = window.location.href;
-  if (domain.includes('localhost:3000') || domain.includes('sanapolku')) {
-    return 'sanapolku/data/paths/adult/images';
-  }
-  return 'public/data/paths/adult/images'; // Path for testing
-}
-
-function kidImagesPath() {
-  const domain = window.location.href;
-  if (domain.includes('localhost:3000') || domain.includes('sanapolku')) {
-    return 'sanapolku/data/paths/kid/images';
-  }
-  return 'public/data/paths/kid/images'; // Path for testing
-}
-
 export function getAdultPath() {
+  const path = 'sanapolku/data/paths/adult/images';
   let words = shuffleArray(adultImageNames);
   if (arguments.length === 1) {
     words = words.slice(0, arguments[0]);
   }
-  const path = adultImagesPath();
   return getPath(words, path);
 }
 
 export function getKidPath() {
+  const path = 'sanapolku/data/paths/kid/images';
   let words = shuffleArray(kidImageNames);
   if (arguments.length === 1) {
     words = words.slice(0, arguments[0]);
   }
-  const path = kidImagesPath();
   return getPath(words, path);
 }
 
