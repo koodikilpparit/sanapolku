ed0fdf4312cf65aea58e6c1e49040ae44f03e524
Neera Kiviluoma <89189068+KiviluomaNeera@users.noreply.github.com>
2024-11-13 00:51:30 +0200

    148 implement flow via path selection for sharing (#201)
    * #148 Luo universal share nappi ja poista väliaikainen share etusivulta
    
    * #148 Lisää jakamiselle modal komponentti
    
    * #148 Lisää Share.js toiminnallisuus PathSelection sivulle
    
    * #148 Korjaa polun export ongelma
    
    * Reject if cannot connect
    
    * Add imported path to state
    
    * #148 Yhdistymisen onnistumisen käsittelyä
    
    * #148 Korjaa bugi polun tupla importtaamisesta
    
    * #148 epic fail bugin korjausyritys
    
    * #148 polun vastaanottaminen pois useEffectistä
    
    * Remove old event handlers
    
    * Remove debug logs
    
    * #148 Refaktoroi polun jakamista
    
    * #148 Poista Share sivu
    
    * #148 Poista turha kommentti
    
    * Revert "#148 Poista turha kommentti"
    
    This reverts commit 2995f74b1db94f7a2031fd68aa62f5b588eb390a.
    
    * Revert "#148 Poista Share sivu"
    
    This reverts commit fb9442864f718ac8ad3b1c2221efe22af89f2bcf.
    
    * Revert "#148 Refaktoroi polun jakamista"
    
    This reverts commit 960c866b235ab48fa961fd8e104b9338586e94e1.
    
    * Epic refactoring :D
    
    * Fixes
    
    * Delete Share page (dev version)
    
    * Fix lint warnings
    
    * Add provider for tests
    
    * Fix tests
    
    * #148 Poista turhat loggaukset
    
    * #148 poista targetPeerId state
    
    * Fix error
    
    ---------
    
    Co-authored-by: Juho Rantala <juho.rantala@live.com>

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -4,7 +4,6 @@ import StartButton from '../components/start/StartButton';
 import HelpButton from '../components/start/HelpButton';
 import LetterTile from '../components/start/LetterTile';
 import './Start.css';
-import ShareButton from '../components/start/ShareButton';
 
 function Start() {
   return (
@@ -33,9 +32,6 @@ function Start() {
         </div>
         <div className="help-button">
           <HelpButton />
-        </div>
-        <div className="share-button">
-          <ShareButton />
         </div>
       </div>
     </div>
