8b78bb0426714519955633f6e93de6a06bba2201
Neera Kiviluoma <neeruska.kiviluoma@gmail.com>
2024-11-14 16:49:54 +0200

    #195 Polun nimen vaihtamisen toiminnallisuus

--- src/components/universal/EditButton.js
+++ src/components/universal/EditButton.js
@@ -2,27 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
-import { useNavigate } from 'react-router-dom';
 import './EditButton.css';
 
-const EditButton = ({ pathId }) => {
-  const navigate = useNavigate();
-
+const EditButton = ({ onClick }) => {
   return (
-    <button
-      className="edit-button"
-      onClick={(e) => {
-        e.stopPropagation();
-        navigate(`/muokkaapolkua/${pathId}`);
-      }}
-    >
+    <button className="edit-button" onClick={onClick}>
       <FontAwesomeIcon icon={faEdit} className="edit-icon" />
     </button>
   );
 };
 
 EditButton.propTypes = {
-  pathId: PropTypes.number.isRequired, // path is required
+  onClick: PropTypes.func.isRequired, // path is required
 };
 
 export default EditButton;
