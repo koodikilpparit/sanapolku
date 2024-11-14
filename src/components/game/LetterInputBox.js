e34ea37f656d9ce51b452a193a7151f1b2f6f40c
Salonj <124079532+Salonj@users.noreply.github.com>
2024-11-15 00:23:37 +0200

    Jaettu letterinputbox omaan komponenttiin

---
+++ src/components/game/LetterInputBox.js
@@ -0,0 +1,94 @@
+import React, { useEffect, useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+
+const LetterInputBox = ({ currentWord, playerInput, setPlayerInput }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    setPlayerInput(Array(currentWord.word.length).fill(''));
+    setActiveIndex(0);
+    setTimeout(() => {
+      if (inputRef.current) inputRef.current.focus();
+    }, 0);
+  }, [currentWord, setPlayerInput]);
+
+  const handleInputChange = (event) => {
+    const value = event.target.value.toUpperCase();
+    if (activeIndex !== null && value) {
+      const newPlayerInput = [...playerInput];
+      newPlayerInput[activeIndex] = value.slice(-1); // Set the typed letter
+      setPlayerInput(newPlayerInput);
+      event.target.value = '';
+
+      if (activeIndex + 1 < currentWord.word.length) {
+        setActiveIndex(activeIndex + 1);
+      }
+    }
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Backspace') {
+      event.preventDefault();
+      const newPlayerInput = [...playerInput];
+
+      if (playerInput[activeIndex] !== '') {
+        // Delete the current letter at activeIndex
+        newPlayerInput[activeIndex] = '';
+        setPlayerInput(newPlayerInput);
+      } else if (activeIndex > 0) {
+        // Move to the previous box and delete the letter there
+        setActiveIndex((prevIndex) => prevIndex - 1);
+        newPlayerInput[activeIndex - 1] = '';
+        setPlayerInput(newPlayerInput);
+      }
+    }
+  };
+
+  const handleLetterBoxClick = (index) => {
+    setActiveIndex(index);
+    setTimeout(() => {
+      if (inputRef.current) inputRef.current.focus();
+    }, 0);
+  };
+
+  return (
+    <div className="flex flex-row gap-1 md:gap-2 items-center justify-center py-4 px-2">
+      {currentWord.word.split('').map((_, index) => (
+        <div
+          key={index}
+          onClick={() => handleLetterBoxClick(index)}
+          className={`w-full aspect-square rounded-lg font-bold text-center text-2xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl p-1 flex items-center justify-center cursor-pointer ${
+            activeIndex === index ? 'bg-sp-light-yellow' : 'bg-sp-white'
+          }`}
+          data-testid="input-box"
+        >
+          {playerInput[index] || ''}
+        </div>
+      ))}
+      <input
+        type="text"
+        ref={inputRef}
+        data-testid="hidden-input"
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+        style={{
+          opacity: 0,
+          position: 'absolute',
+          pointerEvents: 'none',
+        }}
+        autoFocus
+      />
+    </div>
+  );
+};
+
+LetterInputBox.propTypes = {
+  currentWord: PropTypes.shape({
+    word: PropTypes.string.isRequired,
+  }).isRequired,
+  playerInput: PropTypes.array.isRequired,
+  setPlayerInput: PropTypes.func.isRequired,
+};
+
+export default LetterInputBox;
--- src/components/game/Phase2.test.js
+++ src/components/game/Phase2.test.js
@@ -1,12 +1,86 @@
-// Clear all mocks after all tests
-afterAll(() => {
-  jest.clearAllMocks();
-});
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Phase2 from './Phase2';
 
 describe('Phase2 Component', () => {
-  describe('Phase2 Component', () => {
-    test('renders without crashing', () => {
-      expect(true).toBe(true);
+  const mockWord = {
+    word: 'apple',
+    imageData: { src: 'apple.jpg', author: 'Unknown' },
+  };
+  const mockShuffledWord = 'ELPPA';
+  const mockHandleSubmit = jest.fn();
+  const mockSetPlayerInput = jest.fn();
+  const mockPlayerInput = ['A', '', '', '', ''];
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('renders Phase2 component correctly', () => {
+    render(
+      <Phase2
+        currentWord={mockWord}
+        shuffledWord={mockShuffledWord}
+        playerInput={mockPlayerInput}
+        setPlayerInput={mockSetPlayerInput}
+        handleSubmit={mockHandleSubmit}
+      />
+    );
+
+    const header = screen.getByRole('heading', { name: /järjestä kirjaimet/i });
+    expect(header).toBeInTheDocument();
+  });
+
+  it('disables the VALMIS button when player input is incomplete', () => {
+    render(
+      <Phase2
+        currentWord={mockWord}
+        shuffledWord={mockShuffledWord}
+        playerInput={mockPlayerInput}
+        setPlayerInput={mockSetPlayerInput}
+        handleSubmit={mockHandleSubmit}
+      />
+    );
+
+    const button = screen.getByRole('button', { name: /valmis/i });
+    expect(button).toBeDisabled();
+  });
+
+  it('displays the shuffled word correctly', () => {
+    render(
+      <Phase2
+        currentWord={mockWord}
+        shuffledWord={mockShuffledWord}
+        playerInput={mockPlayerInput}
+        setPlayerInput={mockSetPlayerInput}
+        handleSubmit={mockHandleSubmit}
+      />
+    );
+
+    const displayLetterBoxes = screen.getAllByTestId(
+      'display-shuffled-letter-box'
+    );
+    const shuffledLetters = mockShuffledWord.split('');
+    shuffledLetters.forEach((letter, index) => {
+      expect(displayLetterBoxes[index]).toHaveTextContent(letter.toUpperCase());
     });
+  });
+
+  it('calls handleSubmit when VALMIS button is clicked and input is complete', () => {
+    render(
+      <Phase2
+        currentWord={mockWord}
+        shuffledWord={mockShuffledWord}
+        playerInput={['A', 'P', 'P', 'L', 'E']}
+        setPlayerInput={mockSetPlayerInput}
+        handleSubmit={mockHandleSubmit}
+      />
+    );
+
+    const button = screen.getByRole('button', { name: /valmis/i });
+    expect(button).toBeEnabled();
+
+    fireEvent.click(button);
+    expect(mockHandleSubmit).toHaveBeenCalled();
   });
 });
