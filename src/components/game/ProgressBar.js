78a3c1468cfe95ce8125fa914dc076ca3769effe
Anni <anni.k.nieminen@tuni.fi>
2024-11-17 12:41:54 +0200

    #161 Tehty ProgressBar komponentti

---
+++ src/components/game/ProgressBar.js
@@ -0,0 +1,18 @@
+// src/components/ProgressBar.js
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../../styles/ProgressBar.css';
+
+const ProgressBar = ({ progress }) => {
+  return (
+    <div className="progress-bar">
+      <div className="progress" style={{ width: `${progress}%` }}></div>
+    </div>
+  );
+};
+
+ProgressBar.propTypes = {
+  progress: PropTypes.number.isRequired,
+};
+
+export default ProgressBar;
