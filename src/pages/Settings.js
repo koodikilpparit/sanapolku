44605599f248de2d06eba758ddaaf71c83e00cbf
xqrima <risto.majakangas@tuni.fi>
2024-11-20 21:14:05 +0200

    Add license information to Settings page

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -114,6 +114,30 @@ const Settings = () => {
               Juho Rantala, Onni Salomaa, Risto &quot;Reine&quot; Majakangas
             </p>
           </div>
+          <div className="soft-box">
+            <h2>Sovelluksen kehitystiimi:</h2>
+            <p>
+              Anni Nieminen, Eveliina Sundberg, Neera Kiviluoma, Tuuli Järvimaa,
+              Juho Rantala, Onni Salomaa, Risto &quot;Reine&quot; Majakangas
+            </p>
+          </div>
+          <div className="soft-box">
+            <h2>Lisenssi:</h2>
+            <p>
+              Tämä sovellus on avoimen lähdekoodin ohjelmisto ja lisensoitu
+              AGPL-3.0-lisenssillä. Voit käyttää, muokata ja jakaa ohjelmaa
+              lisenssiehtojen mukaisesti.{' '}
+              <a
+                href="https://github.com/koodikilpparit/sanapolku"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ textDecoration: 'underline' }}
+              >
+                Lähdekoodi saatavilla
+              </a>
+              .
+            </p>
+          </div>
         </div>
       </div>
 
