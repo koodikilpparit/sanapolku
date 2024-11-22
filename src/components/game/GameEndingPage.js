b33e2b27c6bd81ea3ab825c1f52ce665df0c2efe
Anni <anni.k.nieminen@tuni.fi>
2024-11-22 17:00:20 +0200

    #215 Tehty loppunäkymälle layout

--- src/components/game/GameEndingPage.js
+++ src/components/game/GameEndingPage.js
@@ -3,7 +3,21 @@ import React from 'react';
 const GameEndingPage = () => {
   return (
     <div className="game-ending-page">
+      <div className="ending-content">
       <h2>Peli ohi!</h2>
+      <p>Saavuit levähdyspaikalle. Nyt on aika levätä hieman ennen kuin matka jatkuu.</p>
+      <p>Pidä pieni tauko ennen kuin jatkat peliä.</p>
+      <img className='ending-img'>
+      </img>
+      <div className="ending-buttons-container">
+        <button className="home-button">
+        </button>
+        <button className="ready-button">
+        </button>
+        <button className="restart-button">
+        </button>
+      </div>
+      </div>
     </div>
   );
 };
