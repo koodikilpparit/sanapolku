c359ab82bfc2fcc6fe962a1367e429ea4c82b9b9
Anni <anni.k.nieminen@tuni.fi>
2024-11-23 11:30:33 +0200

    #215 Korjattu loppunäkymän teksti ja lisätty toiminnallisuus restart-napille

--- src/components/game/GameEndingPage.js
+++ src/components/game/GameEndingPage.js
@@ -1,22 +1,36 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRotateLeft } from '@fortawesome/free-solid-svg-icons';
+import'./GameEndingPage.css';
 
 const GameEndingPage = () => {
+  const navigate = useNavigate();
+
+  const handleRestart = () => {
+    // Navigate to the same path to restart the game
+    navigate(0);
+  };
+
   return (
     <div className="game-ending-page">
       <div className="ending-content">
-      <h2>Peli ohi!</h2>
-      <p>Saavuit levähdyspaikalle. Nyt on aika levätä hieman ennen kuin matka jatkuu.</p>
-      <p>Pidä pieni tauko ennen kuin jatkat peliä.</p>
-      <img className='ending-img'>
-      </img>
-      <div className="ending-buttons-container">
-        <button className="home-button">
-        </button>
-        <button className="ready-button">
-        </button>
-        <button className="restart-button">
-        </button>
-      </div>
+        <h2>Onnittelut!</h2>
+        <p>Suoritit koko polun ja saavuit määränpäähäsi!</p>
+        <img
+        className="ending-img"
+        src="sanapolku/finish-img.png"
+        alt="Cottage in serene lake and mountain view"
+        />
+        <div className="ending-buttons-container">
+          <button className="home-button">
+          </button>
+          <button className="ready-button">
+          </button>
+          <button className="restart-button" onClick={handleRestart}>
+            <FontAwesomeIcon icon={faArrowRotateLeft} className="restart-icon" />
+          </button>
+        </div>
       </div>
     </div>
   );
