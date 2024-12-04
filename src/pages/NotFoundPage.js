105d401bf0ac76d8fab6b1bbe57a72804d3b7718
Majis-R <157882306+Majis-R@users.noreply.github.com>
2024-12-04 20:05:48 +0200

    89 page not found page (#286)
    * Add NotFoundPage component and update routing in App
    
    * Implement navigation to error page on word fetch failure in GameEngine
    
    * Add emoji to NotFoundPage for improved cuteness factor
    
    * Change logic to not navigate to error page when no words found for path
    
    * Revert previous commit

---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const NotFound = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen text-center">
+      <h1 className="text-sp-white text-4xl md:text-6xl lg:text-7xl font-bold py-2 md:py-4">
+        Hups, eksyit polulta! 😅
+      </h1>
+      <button
+        className="btn-sp-primary w-full sm:w-1/2 bg-sp-light-green cursor-pointer"
+        onClick={() => navigate('/')}
+      >
+        Takaisin etusivulle
+      </button>
+    </div>
+  );
+};
+
+export default NotFound;
